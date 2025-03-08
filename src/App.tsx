e2d1c6a20823e38779a81257e21f39a09ef87428
nino-rgb <pnekoneko907@gmail.com>
2025-03-08 15:25:55 +0900

    バックエンド疎通確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { Talents } from "model/talent";
 
-function App() {
+axios.defaults.baseURL = "http://localhost:4000/api/";
+
+const App: React.FC = () => {
+  const [talentList, setTalentList] = useState<Talents[]>([]);
+  // get
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get<Talents[]>("/talents");
+      setTalentList(response.data);
+      console.log(talentList);
+    })();
+  }, [setTalentList]);
+
+  const Talent = () => {
+    return <h1>TEST</h1>;
+  };
   return (
-    <div>
-      <h1>うんちうんちメソポタミア文明ねっ竹内くん</h1>
+    <div className="App">
+      <Talent />
+      {talentList.map((talent: Talents) => {
+        return (
+          <div key={talent.id}>
+            <h1>
+              {talent.name}; {talent.youtube_profile}
+            </h1>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/model/talent.ts
@@ -0,0 +1,6 @@
+export type Talents = {
+  id?: number;
+  name: string;
+  youtube_profile: string;
+  official_profile: string;
+};
